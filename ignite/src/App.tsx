4be79eaea9aa750d3139390672641c762287b6e7
Arthur <arthur.moreira@itec.ufpa.br>
2022-03-30 10:00:00 -0300

    modal config

--- ignite/src/App.tsx
+++ ignite/src/App.tsx
@@ -1,13 +1,37 @@
-import React, { Component }  from 'react';
+import React, { Component, useState }  from 'react';
+import Modal from 'react-modal';
 import { Dashboard } from './components/Dashboard';
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root')
+
 export const App = () => {
+	const [
+		isNewTransactionModal, 
+		setIsNewTransactionModal
+	] = useState(false)
+
+	const handleNewTransactionModalOpen = () => {
+		setIsNewTransactionModal(true)
+	}
+
+	const handleNewTransactionModalClose = () => {
+		setIsNewTransactionModal(false)
+	}
+	
 	return (
 		<>
-			<Header />
+			<Header onOpenNewTransactionModal={
+				handleNewTransactionModalOpen
+			}/>
 			<Dashboard />
+			<Modal 
+				isOpen={isNewTransactionModal}
+				onRequestClose={handleNewTransactionModalClose}
+			>
+				<h2>Cadastrar Transação</h2>
+			</Modal>
 			<GlobalStyle />
 		</>
 	);
--- ignite/src/components/Header/index.tsx
+++ ignite/src/components/Header/index.tsx
@@ -1,13 +1,21 @@
-import React, { Component }  from 'react';
+import React, { Component, useState }  from 'react';
 import logoImg from '../../assets/logo.svg'
+import Modal from 'react-modal'
 import { Container, Content } from './styles';
 
-export const Header = () => {
+interface HeaderProps {
+	onOpenNewTransactionModal: () => void
+}
+
+export const Header = ({ onOpenNewTransactionModal }: HeaderProps) => {
 	return (
 		<Container>
 			<Content>
 				<img src={logoImg} alt="dt money" />
-				<button type="button">
+				<button 
+					type="button"
+					onClick={onOpenNewTransactionModal}
+				>
         Nova transação
 				</button>
 			</Content>
